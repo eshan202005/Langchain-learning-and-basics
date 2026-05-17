d592e0c660f8a33f3c3e175d0e1f481a22656fa3
Eshan Ashwani Gupta <eshangupta.prof@gmail.com>
2026-05-17 14:26:30 +0530

    learned about messages and integrated in chatbox

--- Prompts/chatbox.py
+++ Prompts/chatbox.py
@@ -1,20 +1,23 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 load_dotenv()
 
 
 model=ChatOpenAI(model="gpt-5-mini", temperature=0.9)
 
-chat_history = []
+chat_history = [SystemMessage(content="You are a helpful assistant.")
+
+]
 
 
 while True:
     user_input=input('You:')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() in ['exit', 'quit']:
         print("Exiting chat...")
         break
 
     result=model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print('AI:', result.content)
