a1a2ec82622794bfce9c6538d1cfd56dc5b4b2bd
Eshan Ashwani Gupta <eshangupta.prof@gmail.com>
2026-05-26 14:12:12 +0530

    learning pypdf

--- chains/conditional_chains.py
+++ chains/conditional_chains.py
@@ -4,7 +4,8 @@ from typing import Literal
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel , RunnableBranch , RunnableLambda
+
 
 
 load_dotenv()
@@ -13,16 +14,32 @@ parser= StrOutputParser()
 
 model= ChatOpenAI(model="gpt-5-mini")
 
-class response(BaseModel):
+class feedback(BaseModel):
     sentiment: Literal['positive', 'negative'] = Field(description="The sentiment of the given text")
 prompt1= PromptTemplate.from_template(
     "classify the sentiment of the following text into positive or negative:\n {text}")
 
-model1= model.with_structured_output(response)
+model1= model.with_structured_output(feedback)
 
 classifier_chain= prompt1 | model1
 
-result=classifier_chain.invoke({"text": "I am very happy with the service provided!"}).sentiment
 
+prompt2= PromptTemplate.from_template(
+    "generate a response based on positive sentiment for the following text:\n {text}")
+
+
+prompt3= PromptTemplate.from_template(
+    "generate a response based on negative sentiment for the following text:\n {text}")
+
+
+
+Branching_chain= RunnableBranch(
+    (lambda x : x.sentiment == 'positive',prompt2|model1),
+    (lambda x : x.sentiment == 'negative',prompt3|model1),
+    RunnableLambda(lambda x : "cannot find sentiment")
+
+)
+chain = classifier_chain | Branching_chain
+result= chain.invoke({"text": "I am very happy with the service provided by your company."})
 
 print(result)
---
+++ runnables/runnable_passthrough.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+prompt1 = PromptTemplate.from_template(
+    'Write a joke about {topic}'
+
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate.from_template(
+    'Explain the following joke - {text}'
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
